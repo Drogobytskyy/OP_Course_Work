4632739914f06c947e15e62cb2362cf531708be3
Drogobytskyy <drogobytskyy.volodymyr@lll.kpi.ua>
2025-04-27 00:11:39 +0300

    bye_temp_file

--- Makefile
+++ Makefile
@@ -1,11 +1,7 @@
 
 
-
 CXX = g++
 CXXFLAGS = -Wall -Wextra -Wpedantic -std=c++17
 
-myprog: task2.cpp start_instruction.cpp
-	$(CXX) $(CXXFLAGS) -o myprog task2.cpp start_instruction.cpp
-
-myalg: algorithm.cpp globals.cpp ship_struct.cpp game_zone.cpp
-	$(CXX) $(CXXFLAGS) -o myalg algorithm.cpp globals.cpp ship_struct.cpp game_zone.cpp
+course: task2.cpp start_instruction.cpp globals.cpp ship_struct.cpp game_zone.cpp
+	$(CXX) $(CXXFLAGS) -o course task2.cpp start_instruction.cpp globals.cpp ship_struct.cpp game_zone.cpp
--- task2.cpp
+++ task2.cpp
@@ -4,7 +4,11 @@
 #include <algorithm>
 #include <cctype>
 #include <cstdlib>
+
 #include "start_instruction.h"
+#include "globals.h"
+#include "ship_struct.h"
+#include "game_zone.h"
 
 using namespace std;
 
@@ -35,6 +39,7 @@ int main(void) {
     cout << endl;
 
     Start_instruction instruction;
+    Game game;
     instruction.show();  
     
     string choice = get_valid_input(instruction);
@@ -44,10 +49,28 @@ int main(void) {
         instruction.custom_message("Let's start!");
         instruction.custom_only_text("So, let's look what field do we have!");
         instruction.up_and_bot();
+        game.print(g_play_zone);
+        int temp_zone[7][9]; 
+        game.copy_zone(g_play_zone, temp_zone);
+        int ships_placed_count = 0;
+        if (game.place_all_ships(temp_zone, game.ship_list, 0, ships_placed_count)) {
+            cout << "Ships placed successfully!" << endl;
+        } else {
+            cout << "ERROR." << endl;
+        }
+        game.print(temp_zone);
+        cout << "Total ships placed: " << game.get_ship_placing_counter() << endl;
+        cout << "Total ships deleted: " << game.get_ship_deleting_counter() << endl;
+        cout << endl;
+
+
     }else{
         instruction.custom_message("Goodbye! Hope we will meet again!");
         return 0;
     }
+
+  
+
     cout << endl;
     return 0;
 }
